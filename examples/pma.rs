ae705b8c97e875a1be81bc039a9bdc5efdf8742d
Louis Boulanger <louis.boulanger74@hotmail.fr>
2019-07-03 17:04:52 +0200

    Optimized from_iterator function
    The initial density will always be 0.5 ± 0.225.

--- examples/pma.rs
+++ examples/pma.rs
@@ -1,13 +1,8 @@
 use pma::PMA;
 
 fn main() {
-    let pma = PMA::from_iterator(0u32..21, 0.3..0.7, 0.08..0.92);
-
-    eprintln!("{:?}", pma.elements_with_holes());
+    for i in 1..2060 {
+        let pma = PMA::from_iterator(0u32..i, 0.3..0.7, 0.08..0.92);
+        println!("{} {}", i, pma.density());
+    }
 }
-
-// n = nombre elems = 5
-// P = taille vec
-// t = ceil(log2 2n) = 4
-// ceil((2n/t)).next_power_of_two -> 2^h = 4
-// 2^h * t -> taille P = 20
--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,7 @@ pub struct PMA<T: FakeOption> {
     data: Vec<T>,
     pma_bounds: Range<f64>,
     segments_bounds: Range<f64>,
+    segment_size: usize,
 }
 
 impl<T: FakeOption> PMA<T> {
@@ -41,12 +42,21 @@ impl<T: FakeOption> PMA<T> {
         I: ExactSizeIterator<Item = T>,
     {
         let element_count = iterator.len();
-        let segment_size = (2.0 * element_count as f64).log2().ceil() as usize;
-        println!("segment size: {}", segment_size);
+        let segment_size =
+            ((2.0 * element_count as f64).log2().ceil() as usize).next_power_of_two();
 
-        let segment_count = (((2.0 * element_count as f64) / segment_size as f64).ceil() as usize)
-            .next_power_of_two();
-        println!("segment count : {}", segment_count);
+        let segment_count_upper =
+            (((2.0 * element_count as f64) / segment_size as f64) as usize).next_power_of_two();
+        let segment_count_lower = segment_count_upper / 2;
+
+        let density_upper = element_count as f64 / (segment_size * segment_count_upper) as f64;
+        let density_lower = element_count as f64 / (segment_size * segment_count_lower) as f64;
+
+        let segment_count = if (density_upper - 0.5).abs() < (density_lower - 0.5).abs() {
+            segment_count_upper
+        } else {
+            segment_count_lower
+        };
 
         let mut data: Vec<T> = Vec::new();
 
@@ -71,7 +81,20 @@ impl<T: FakeOption> PMA<T> {
             data,
             pma_bounds,
             segments_bounds,
+            segment_size,
         }
+    }
+
+    fn element_count(&self) -> usize {
+        self.data.iter().filter(|e| !e.is_none()).count()
+    }
+
+    fn segment_size(&self) -> usize {
+        (2.0 * self.element_count() as f64).log2().ceil() as usize
+    }
+
+    fn segment_count(&self) -> usize {
+        ((2.0 * self.element_count() as f64) / self.segment_size() as f64).ceil() as usize
     }
 
     pub fn elements(&self) -> Vec<&T> {
@@ -80,5 +103,9 @@ impl<T: FakeOption> PMA<T> {
 
     pub fn elements_with_holes(&self) -> Vec<&T> {
         self.data.iter().collect()
+    }
+
+    pub fn density(&self) -> f64 {
+        self.element_count() as f64 / self.data.len() as f64
     }
 }
